2d00c8f569b8ccfbb480fad73a6bce7476ce44e4
Julia Kafarska <julia.kafarska@gmail.com>
2024-04-25 13:40:38 +0200

    ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-function App() {
-  const text = ''
+import { useEffect, useState } from 'react'
+import { numberFromWord } from './utils/numberFromWord'
 
-  return (
-    <div>
-      <input type='text' />
-      <div>
-        <p>
-          Output: {text}
-        </p>
-      </div>
-    </div>
-  )
+function App() {
+    const [value, setValue] = useState('one hundred twenty and one')
+    const [result, setResult] = useState(0)
+
+    useEffect(() => {
+        setResult(numberFromWord(value))
+    }, [value])
+
+    return (
+        <div>
+            <input
+                type="text"
+                defaultValue={value}
+                onChange={(e) => {
+                    setValue(e.target.value)
+                }}
+            />
+            <div>
+                <p>Output: {result}</p>
+            </div>
+        </div>
+    )
 }
 
 export default App
